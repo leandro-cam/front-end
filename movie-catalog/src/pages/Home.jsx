8fa14d7f1f9cbcac485db00eb4a92bea0c759087
Leandro Campos <camposleandro748@gmail.com>
2024-01-21 20:39:40 -0300

    feat: created Home page

---
+++ movie-catalog/src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import { MovieCard } from "../components/MovieCard";
+
+const TMDB_API_URL = import.meta.env.VITE_TMDB_API_URL;
+const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+  const getTopMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topMoviesUrl = `${TMDB_API_URL}/top_rated?api_key=${TMDB_API_KEY}`;
+    getTopMovies(topMoviesUrl);
+  }, []);
+
+  const MoviesContainer = () => {
+    const isLoading = topMovies.length === 0;
+
+    return (
+      <div className="movies-container">
+        {isLoading && <p>Carregando...</p>}
+        {!isLoading &&
+          topMovies.map((movie) => (
+            <p key={movie.id}>
+              <MovieCard movie={movie} />
+            </p>
+          ))}
+      </div>
+    );
+  };
+
+  return (
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <MoviesContainer />
+    </div>
+  );
+};
+
+export default Home;
