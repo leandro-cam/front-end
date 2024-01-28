644a6b7e033462b40e01b123a10ec6786c3cb255
Leandro Campos <camposleandro748@gmail.com>
2024-01-28 10:34:15 -0300

    fix: added dependency array in useEffect to fix looping

--- movie-catalog/src/pages/Movie.jsx
+++ movie-catalog/src/pages/Movie.jsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { BsWallet2, BsGraphUp, BsHourglassSplit, BsFillFileEarmarkTextFill } from "react-icons/bs"
+import {
+  BsWallet2,
+  BsGraphUp,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill,
+} from "react-icons/bs";
 
-import {MovieCard} from "../components/MovieCard"
+import { MovieCard } from "../components/MovieCard";
 
-import "./Movie.css"
+import "./Movie.css";
 
 const TMDB_API_URL = import.meta.env.VITE_TMDB_API_URL;
 const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 export const Movie = () => {
-  const {id} = useParams()
-  const [movie, setMovie] = useState()
+  const { id } = useParams();
+  const [movie, setMovie] = useState();
 
   const getMovie = async (url) => {
     const res = await fetch(url);
@@ -21,47 +26,53 @@ export const Movie = () => {
   };
 
   const formatNumberToDollar = (value) => {
-    return value.toLocaleString('en-US', {
-      style: 'currency',
-      currency: 'USD'
-    })
-  }
+    return value.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
 
   useEffect(() => {
     const movieUrl = `${TMDB_API_URL}/${id}?api_key=${TMDB_API_KEY}`;
     getMovie(movieUrl);
-  })
+  }, [id]);
 
-  return <div className="movie-detail">{movie && (<>
-    <MovieCard movie={movie} showLink={false}></MovieCard>
-    <p className="tagline">{movie.tagline}</p>
-    <div className="info">
-      <h3>
-        <BsWallet2/>
-        Orçamento:
-      </h3>
-      <p>{formatNumberToDollar(movie.budget)}</p>
+  return (
+    <div className="movie-detail">
+      {movie && (
+        <>
+          <MovieCard movie={movie} showLink={false}></MovieCard>
+          <p className="tagline">{movie.tagline}</p>
+          <div className="info">
+            <h3>
+              <BsWallet2 />
+              Orçamento:
+            </h3>
+            <p>{formatNumberToDollar(movie.budget)}</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsGraphUp />
+              Faturamento:
+            </h3>
+            <p>{formatNumberToDollar(movie.revenue)}</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsHourglassSplit />
+              Duração:
+            </h3>
+            <p>{movie.runtime} minutos</p>
+          </div>
+          <div className="info description">
+            <h3>
+              <BsFillFileEarmarkTextFill />
+              Descrição:
+            </h3>
+            <p>{movie.overview}</p>
+          </div>
+        </>
+      )}
     </div>
-    <div className="info">
-      <h3>
-        <BsGraphUp/>
-        Faturamento:
-      </h3>
-      <p>{formatNumberToDollar(movie.revenue)}</p>
-    </div>
-    <div className="info">
-      <h3>
-        <BsHourglassSplit/>
-        Duração:
-      </h3>
-      <p>{movie.runtime} minutos</p>
-    </div>
-    <div className="info description">
-      <h3>
-        <BsFillFileEarmarkTextFill/>
-        Descrição:
-      </h3>
-      <p>{movie.overview}</p>
-    </div>
-  </>)}</div>;
+  );
 };
