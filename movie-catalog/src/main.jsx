5493564dbbc25c22fe18e9d6a1cc90ffa488376e
Leandro Campos <camposleandro748@gmail.com>
2024-01-28 05:58:16 -0300

    fix: updated router base path to '/front-end/movie-catalog'

--- movie-catalog/src/main.jsx
+++ movie-catalog/src/main.jsx
@@ -11,7 +11,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/front-end/movie-catalog">
       <Routes>
         <Route element={<App />}>
           <Route path="/" element={<Home />} />
