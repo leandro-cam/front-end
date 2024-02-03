a7d101047e1121feaf162dd497ff57ea7ae7c184
Leandro Campos <camposleandro748@gmail.com>
2024-02-02 21:49:26 -0300

    feat: added genres in Navbar

--- movie-catalog/src/components/Navbar.jsx
+++ movie-catalog/src/components/Navbar.jsx
@@ -1,22 +1,63 @@
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import { BiCameraMovie, BiSearchAlt2, BiX } from "react-icons/bi";
 import { Link, useNavigate } from "react-router-dom";
-import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
-import { useState } from "react";
 
 import "./Navbar.css";
 
+import { GENRE_IN_PORTUGUESE_TO_GENRE_ID } from "../constants/constants";
+
 export const Navbar = () => {
-  const [search, setSearch] = useState('')
-  const navigate = useNavigate()
+  const [search, setSearch] = useState("");
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    if (!search) return
+    if (!search) return;
 
-    navigate(`/search?s=${search}`)
+    navigate(`/search?s=${search}`);
 
-    setSearch('')
-  }
+    setSearch("");
+  };
+
+  const GenreButton = ({ genreName }) => {
+    const isSelectedGenre = selectedGenres.includes(genreName);
+    const onSelect = (genreName) => {
+      setSelectedGenres([...selectedGenres, genreName]);
+    };
+    const onDeselect = (genreName) => {
+      const updatedGenres = selectedGenres.filter(
+        (genre) => genre !== genreName
+      );
+      setSelectedGenres(updatedGenres);
+      if (!updatedGenres.length) navigate(`/`);
+    };
+
+    return (
+      <button
+        className={`genre-button ${isSelectedGenre ? "selected-genre" : ""}`}
+        onClick={
+          isSelectedGenre
+            ? () => onDeselect(genreName)
+            : () => onSelect(genreName)
+        }
+      >
+        {genreName}
+        {isSelectedGenre && <BiX />}
+      </button>
+    );
+  };
+
+  GenreButton.propTypes = {
+    genreName: PropTypes.string.isRequired,
+  };
+
+  useEffect(() => {
+    if (!selectedGenres.length) return;
+    navigate(`/search-genre?genres=${selectedGenres.join()}`);
+  }, [selectedGenres]);
 
   return (
     <nav id="navbar">
@@ -27,8 +68,19 @@ export const Navbar = () => {
         </Link>
       </h2>
 
+      <div className="genres">
+        {Object.keys(GENRE_IN_PORTUGUESE_TO_GENRE_ID).map((genreName) => (
+          <GenreButton key={genreName} genreName={genreName} />
+        ))}
+      </div>
+
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Busque um filme" onChange={(e) => setSearch(e.target.value)} value={search} />
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
         <button type="submit">
           <BiSearchAlt2 />
         </button>
