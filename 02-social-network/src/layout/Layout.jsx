058eceb083016c2aa7ca510c276ac7d0030e8f1c
Leandro Campos <camposleandro748@gmail.com>
2024-02-13 14:44:09 -0300

    feat: created pages in routes and menus

--- 02-social-network/src/layout/Layout.jsx
+++ 02-social-network/src/layout/Layout.jsx
@@ -1,11 +1,50 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
-// Pages
+import Home from "../pages/home/Home";
+import Profile from "../pages/profile/Profile";
+import ChatBox from "../pages/chat-box/ChatBox";
 import Login from "../pages/login/Login";
 import SignUp from "../pages/sign-up/SignUp";
 
+import Nav from "../components/nav/Nav";
+import LeftBar from "../components/left-bar/LeftBar";
+import RightBar from "../components/right-bar/RightBar";
+
 export default function Layout() {
+  const Feed = () => {
+    return (
+      <>
+        <Nav />
+        <main>
+          <LeftBar />
+          <div className="container">
+            <Outlet />
+          </div>
+          <RightBar />
+        </main>
+      </>
+    );
+  };
+
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Feed />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+        {
+          path: "/chat-box/:id",
+          element: <ChatBox />,
+        },
+      ],
+    },
     {
       path: "/login",
       element: <Login />,
