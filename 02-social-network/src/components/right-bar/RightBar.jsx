9b23f2ead34a73f692b8b141bc84e04b6467b8fd
Leandro Campos <camposleandro748@gmail.com>
2024-02-18 17:28:07 -0300

    feat: created RightBar component with Messages and FriendRequests

--- 02-social-network/src/components/right-bar/RightBar.jsx
+++ 02-social-network/src/components/right-bar/RightBar.jsx
@@ -1,3 +1,15 @@
+import Messages from "../messages/Messages";
+import FriendRequests from "../friend-requests/FriendRequests";
+
+import "./right-bar.css";
+
 export default function RightBar() {
-  return <div>RightBar</div>;
+  return (
+    <div className="right-bar">
+      <div className="right-bar-container">
+        <Messages />
+        <FriendRequests />
+      </div>
+    </div>
+  );
 }
