d4e5366cb018a4651a8183b718d9c627fbbc3a3b
Leandro Campos <camposleandro748@gmail.com>
2024-02-18 17:27:07 -0300

    fix: updated texts of friends fake api

--- 02-social-network/src/fake-apis/friends.js
+++ 02-social-network/src/fake-apis/friends.js
@@ -3,31 +3,31 @@ import FriendImage2 from "../assets/imgs/FirendReq1.webp";
 import FriendImage3 from "../assets/imgs/FirendReq2.jpg";
 import FriendImage4 from "../assets/imgs/FirendReq3.jpg";
 
-const Friends = [
+const friendRequests = [
   {
     id: 1,
     name: "Anna Loise",
     img: FriendImage1,
-    info: " 4 mutual Following",
+    info: " 4 mutual friends",
   },
   {
     id: 2,
     name: "Eva Gacorai",
     img: FriendImage2,
-    info: " 4 mutual Following",
+    info: " 4 mutual friends",
   },
   {
     id: 3,
     name: "Anny Joly",
     img: FriendImage3,
-    info: " 4 mutual Following",
+    info: " 4 mutual friends",
   },
   {
     id: 4,
     name: "Prince Adam",
     img: FriendImage4,
-    info: " 4 mutual Following",
+    info: " 4 mutual friends",
   },
 ];
 
-export default Friends;
+export default friendRequests;
