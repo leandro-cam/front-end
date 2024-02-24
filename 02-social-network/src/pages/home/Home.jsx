d89d64606679a14cce5648fd6e3845ac41ebc88b
Leandro Campos <camposleandro748@gmail.com>
2024-02-24 20:32:18 -0300

    feat: Created Home

--- 02-social-network/src/pages/home/Home.jsx
+++ 02-social-network/src/pages/home/Home.jsx
@@ -1,3 +1,13 @@
+import Stories from "../../components/stories/Stories";
+import AddPost from "../../components/add-post/AddPost";
+import Feeds from "../../components/feeds/Feeds";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Stories />
+      <AddPost />
+      <Feeds />
+    </div>
+  );
 }
