2cd9b422093ccda3e1508bfda1835204fdea61f3
Leandro Campos <camposleandro748@gmail.com>
2024-02-25 11:33:17 -0300

    feat: Created ChatBox page

--- 02-social-network/src/pages/chat-box/ChatBox.jsx
+++ 02-social-network/src/pages/chat-box/ChatBox.jsx
@@ -1,3 +1,43 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowAltCircleRight,
+  faFileAlt,
+} from "@fortawesome/free-solid-svg-icons";
+
+import Stories from "../../components/stories/Stories";
+
+import currentUser from "../../fake-apis/current-user";
+
+import "./chat-box.css";
+
 export default function ChatBox() {
-  return <div>ChatBox</div>;
+  return (
+    <div>
+      <Stories />
+
+      <div className="chat-box">
+        <div className="chat-box-top">
+          <img src={currentUser.profileImage} alt="" />
+
+          <div className="user-name">
+            <h3>{currentUser.name}</h3>
+            <h5>{currentUser.username}</h5>
+          </div>
+        </div>
+
+        <div className="chat-box-bottom">
+          <form action="#">
+            <input type="text" placeholder="Write something" />
+            <button type="submit" className="btn btn-primary">
+              <FontAwesomeIcon icon={faArrowAltCircleRight} />
+            </button>
+            <label className="btn btn-primary" htmlFor="chat-box-file">
+              <FontAwesomeIcon icon={faFileAlt} />
+              <input type="file" id="chat-box-file" />
+            </label>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
